7181853fb2527d8b3e7d79a61a9e13a021ab8b31
KrzysztofDolanski <k.dolanski@wp.pl>
2021-02-21 22:14:46 +0100

    Document Entity

--- src/main/java/com/example/formatter_adapter/entities/User.java
+++ src/main/java/com/example/formatter_adapter/entities/User.java
@@ -11,4 +11,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+
 }
---
+++ src/main/java/com/example/formatter_adapter/repositories/DocumentRepository.java
@@ -0,0 +1,9 @@
+package com.example.formatter_adapter.repositories;
+
+import com.example.formatter_adapter.entities.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+}
